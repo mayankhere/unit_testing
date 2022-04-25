4383633bc70c0c62d5b6f6dd44fcbdded265e632
Mayank Naithani <mayankhere@gmail.com>
2022-04-25 19:31:59 +0530

    added multiple tests to the file

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', function() {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 2 + 2 to equal 4', function() {
+  expect(sum(2, 2)).toBe(4);
+});
